a563b0e60a93ee0d4e01688b1ceeebb0f9b1f392
Borisov Alexey <borisov@lcard.ru>
2015-09-17 20:25:37 +0300

    ltrsrvcon: ????????? ?????? ??? ?????? ? IP-???????? ? ?????? ????????? ???? ???????

---
+++ examples/ltrapi_iprec/ltrapi_iprec.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Net;
+using ltrModulesNet;
+
+
+
+/* Данный пример демонстрирует получение списка всех IP-записей */
+namespace ltrapi_iprec
+{
+    class ltrapi_iprec
+    {
+
+        static int Main(string[] args)
+        {
+            /* для передачи управляющих команд устанавливаем управляющее соединение
+             * с сервисом (ltrd/LtrServer). Для управляющего соединения существует
+             * специальный класс ltrsrvcon с набором функций ltrapi, которые относятся
+             * к командом управления сервисом */
+            ltrsrvcon srvcon = new ltrsrvcon();
+            _LTRNative.LTRERROR err = srvcon.Open();
+            if (err != _LTRNative.LTRERROR.OK)
+            {
+                Console.WriteLine("Не удалось установить связь с сервисом. Ошибка {0}: {1}",
+                                  err, ltrsrvcon.GetErrorString(err));
+            }
+            else
+            {
+                ltrsrvcon.IpCrateEntry[] ipEntries;
+                /* получаем все записи */
+                err = srvcon.GetListOfIPCrates(0, 0, out ipEntries);
+                if (err != _LTRNative.LTRERROR.OK)
+                {
+                    Console.WriteLine("Не получить список IP-записей. Ошибка {0}: {1}",
+                                      err, ltrsrvcon.GetErrorString(err));
+                }
+                else
+                {
+                    Console.WriteLine("Найдено IP-записей: {0}", ipEntries.Length);
+                    for (int i = 0; i < ipEntries.Length; i++)
+                    {
+                        Console.WriteLine("Запись {0}: {1}, {2}, S/N: {3}, Auto: {4}", i + 1, ipEntries[i].IpAddr, 
+                            ipEntries[i].Status, ipEntries[i].CrateSerial, ipEntries[i].Autoconnect);                        
+                    }
+                    
+
+                }
+            }
+
+
+
+            if (srvcon.IsOpened() == _LTRNative.LTRERROR.OK)
+                srvcon.Close();
+
+            return (int)err;
+        }
+    }
+}
--- ltrsrvcon.cs
+++ ltrsrvcon.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
-
+using System.Net;
 
 namespace ltrModulesNet
 {
@@ -103,6 +103,149 @@ namespace ltrModulesNet
             return _LTRNative.LTR_GetServerVersion(ref hnd, out ver);
         }
 
+
+        static uint ipAddrToUint(IPAddress addr)
+        {
+            byte[] bytes = addr.GetAddressBytes();
+            Array.Reverse(bytes); // flip big-endian(network order) to little-endian
+            return BitConverter.ToUInt32(bytes, 0);
+        }
+
+        static IPAddress uintToIpAddr(uint intAddr)
+        {
+            byte[] bytes = BitConverter.GetBytes(intAddr);
+            Array.Reverse(bytes);
+            return new IPAddress(bytes);
+        }
+
+        // элемент списка IP-крейтов
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        public struct IpCrateEntry
+        {
+            public enum IpStatus : byte
+            {
+                Offline = 0,
+                Connecting = 1,
+                Online = 2,
+                Error = 3
+            };
+
+            [Flags]
+            public enum IpFlags : uint
+            {
+                Autoconnect = 0x00000001
+            }
+
+
+            uint _ip_addr;                                          // IP адрес (host-endian)
+            IpFlags _flags;                                            // флаги режимов (CRATE_IP_FLAG_...)
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            char[] _serial_number;                                 // серийный номер (если крейт подключен)
+            byte _is_dynamic;                                        // 0 = задан пользователем, 1 = найден автоматически
+            IpStatus _status;                                            // состояние (CRATE_IP_STATUS_...)
+
+
+            public IPAddress IpAddr { get { return uintToIpAddr(_ip_addr); } }
+            public uint IpAddrUint { get { return _ip_addr; } }
+            public String CrateSerial { get { return new string(_serial_number).Split('\0')[0]; } }
+            public IpStatus Status { get { return _status; } }
+            public IpFlags Flags { get { return _flags; } }
+            public bool Autoconnect { get { return (_flags & IpFlags.Autoconnect) != 0; } }
+        }
+
+
+        public virtual _LTRNative.LTRERROR GetListOfIPCrates(UInt32 ip_net, UInt32 ip_mask, out IpCrateEntry[] info_array)
+        {
+            UInt32 fnd, ret;
+            // info_array = new TIPCRATE_ENTRY[0];
+            //   TIPCRATE_ENTRY info = new TIPCRATE_ENTRY();
+            IntPtr ptr = IntPtr.Zero;
+            _LTRNative.LTRERROR res = _LTRNative.LTR_GetListOfIPCrates(ref hnd, 0, ip_net, ip_mask, out fnd, out ret, ptr);
+
+            if (res == _LTRNative.LTRERROR.OK)
+            {
+                if (fnd > 0)
+                {
+                    int size = Marshal.SizeOf(typeof(IpCrateEntry));
+                    ptr = Marshal.AllocHGlobal(size * (int)fnd);
+                    res = _LTRNative.LTR_GetListOfIPCrates(ref hnd, fnd, ip_net, ip_mask, out fnd, out ret, ptr);
+
+                    info_array = new IpCrateEntry[fnd];
+
+                    for (int i = 0; i < fnd; i++)
+                    {
+                        IntPtr infoptr = new IntPtr(ptr.ToInt32() + size * i);
+                        info_array[i] = new IpCrateEntry();
+                        info_array[i] = (IpCrateEntry)Marshal.PtrToStructure(infoptr, typeof(IpCrateEntry));
+                    }
+
+                    Marshal.FreeHGlobal(ptr);
+                }
+                else
+                    info_array = new IpCrateEntry[0];
+
+            }
+            else
+                info_array = new IpCrateEntry[0];
+
+            return res;
+        }
+
+        public virtual _LTRNative.LTRERROR AddIPCrate(uint ip_addr, IpCrateEntry.IpFlags flags, bool permanent)
+        {
+            return _LTRNative.LTR_AddIPCrate(ref hnd, ip_addr, (uint)flags, permanent);
+        }
+
+        public virtual _LTRNative.LTRERROR AddIPCrate(IPAddress ip_addr, IpCrateEntry.IpFlags flags, bool permanent)
+        {
+            return _LTRNative.LTR_AddIPCrate(ref hnd, ipAddrToUint(ip_addr), (uint)flags, permanent);
+        }
+
+        public virtual _LTRNative.LTRERROR DeleteIPCrate(uint ip_addr, bool permanent)
+        {
+            return _LTRNative.LTR_DeleteIPCrate(ref hnd, ip_addr, permanent);
+        }
+
+        public virtual _LTRNative.LTRERROR DeleteIPCrate(IPAddress ip_addr, bool permanent)
+        {
+            return _LTRNative.LTR_DeleteIPCrate(ref hnd, ipAddrToUint(ip_addr), permanent);
+        }
+
+        public virtual _LTRNative.LTRERROR ConnectIPCrate(uint ip_addr)
+        {
+            return _LTRNative.LTR_ConnectIPCrate(ref hnd, ip_addr);
+        }
+
+        public virtual _LTRNative.LTRERROR ConnectIPCrate(IPAddress ip_addr)
+        {
+            return _LTRNative.LTR_ConnectIPCrate(ref hnd, ipAddrToUint(ip_addr));
+        }
+
+        public virtual _LTRNative.LTRERROR DisconnectIPCrate(uint ip_addr)
+        {
+            return _LTRNative.LTR_DisconnectIPCrate(ref hnd, ip_addr);
+        }
+
+        public virtual _LTRNative.LTRERROR DisconnectIPCrate(IPAddress ip_addr)
+        {
+            return _LTRNative.LTR_DisconnectIPCrate(ref hnd, ipAddrToUint(ip_addr));
+        }
+
+        public virtual _LTRNative.LTRERROR ConnectAllAutoIPCrates()
+        {
+            return _LTRNative.LTR_ConnectAllAutoIPCrates(ref hnd);
+        }
+
+        public virtual _LTRNative.LTRERROR DisconnectAllIPCrates()
+        {
+            return _LTRNative.LTR_DisconnectAllIPCrates(ref hnd);
+        }
+
+        public virtual _LTRNative.LTRERROR SetIPCrateFlags(uint ip_addr, IpCrateEntry.IpFlags new_flags, bool permanent)
+        {
+            return _LTRNative.LTR_SetIPCrateFlags(ref hnd, ip_addr, (uint)new_flags, permanent);
+        }
+
         public static string GetErrorString(_LTRNative.LTRERROR err)
         {
             IntPtr ptr = LTR_GetErrorString((int)err);
@@ -111,6 +254,7 @@ namespace ltrModulesNet
             Encoding dstEncodingFormat = Encoding.UTF8;
             return dstEncodingFormat.GetString(Encoding.Convert(srcEncodingFormat, dstEncodingFormat, srcEncodingFormat.GetBytes(str)));
         }
+
 
       
         _LTRNative.TLTR hnd;
